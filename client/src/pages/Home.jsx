2953b834849d070377d2650a16c7f05ea3ab78bf
Ahmad Qaher <sqaher@my.centennialcollege.ca>
2025-07-29 23:40:16 -0400

    updated main

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,6 +6,8 @@ const Home = () => {
             <p>Here you can find information about me, my projects, the services I offer and how to contact me.</p>
             <h3>Mission Statement</h3>
             <p>To make my experiences and skills your tool to success. I aim to provide high-quality software solutions that meet your needs and exceed your expectations.</p>
+            <h3>Vision Statement</h3>
+            <p>To be a leading software developer known for innovation, quality, and customer satisfaction. I strive to create software that not only solves problems but also enhances the user experience.</p>
         </div>
     );
 };
